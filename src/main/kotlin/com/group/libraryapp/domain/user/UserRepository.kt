d8a631e6fa56e272e46aba8a26fec33036b7cbae
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-12-11 00:16:47 +0900

    feat: querydsl를 이용하여 쿼리 메서드 변경

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,12 +1,11 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
 
   fun findByName(name: String) : User?
 
-  @Query("select distinct u from User u left join u.userLoanHistory ulh")
-  fun findAllWithHistories(): List<User>
+//  @Query("select distinct u from User u left join u.userLoanHistory ulh")
+//  fun findAllWithHistories(): List<User>
 }
